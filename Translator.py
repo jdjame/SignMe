7f2af690e201aba0a6c53f96edd3b8ffac5a2e4b
jdjame <jean-yves.djamen-kepaou@mail.mcgill.ca>
2018-08-25 20:29:33 -0400

    Added translator

--- Translator.py
+++ Translator.py
@@ -1,2 +1,34 @@
-print("Please enter what you would like translated")
-#Now read the textcd
+import sys
+
+from PIL import Image
+import numpy as np
+
+sentence= input("Please enter what you would like translated \n").lower()
+
+alph_array=[]
+
+#get list of all hand movements used in the word
+for x in sentence:
+    if x == ' ' :
+        alph_array.append('Alphabet/space.png')
+    else:
+        alph_array.append('Alphabet/' + x + '.png')
+print (alph_array)
+
+#list of all images in our hand array
+images= [Image.open(i) for i in alph_array]
+
+#for t in images:
+#    t.show()
+
+sizeAr=[]
+for t in images:
+    sizeAr.append(t.size)
+
+smallest= min(sizeAr)
+
+print(smallest)
+
+for t in images:
+    t.resize(smallest)
+    print(t.size)
